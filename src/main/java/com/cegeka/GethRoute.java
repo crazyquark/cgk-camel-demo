03cb780efe51d52ee0d4110e222e174691d69f3d
crazyquark <cristian.sandu@gmail.com>
2016-06-22 14:26:54 +0300

    more fun

--- src/main/java/com/cegeka/GethRoute.java
+++ src/main/java/com/cegeka/GethRoute.java
@@ -1,15 +1,18 @@
 package com.cegeka;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.http4.HttpMethods;
 import org.springframework.stereotype.Component;
  
 @Component
-public class MyRoute extends RouteBuilder {
+public class GethRoute extends RouteBuilder {
  
     @Override
     public void configure() throws Exception {
         from("direct:geth")
-//        .transform(method("myBean", "saySomething"))
+        .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
         .to("http4://localhost:8545")
         .to("stream:out");
     }
